db5acd676a2f36450c071e2a06895d41c6fec66d
VirtualBrightPlayz <lego.trains.b@gmail.com>
2020-08-10 18:41:08 -0700

    Initial commit

---
+++ Knife/Knife/KnifeItem.cs
@@ -0,0 +1,104 @@
+﻿using EXILED;
+using EXILED.Extensions;
+using ItemManager;
+using Mirror;
+using PlayableScps;
+using RemoteAdmin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Knife
+{
+    public class KnifeItem : CustomItem
+    {
+        private const int PlayerLayerMask = 1208246273;
+        /*public override int MagazineCapacity => 9999;
+
+        public override float FireRate => 1f;*/
+
+        /*public KnifeItem(PluginMain pl)
+        {
+            plugin = pl;
+        }*/
+
+        public override void OnMedkitUse()
+        {
+            // Nullify damage from straight shot
+            float damage = 0; // todo: this causes everything to be a oneshot
+
+            Log.Info("A KNIFE!!!");
+            PlayerObject.GetPlayer().SetCurrentItem(ItemType.None);
+            PlayerObject.GetPlayer().SetCurrentItem(ItemType.Adrenaline);
+
+            Transform cam = PlayerObject.GetComponent<ReferenceHub>().PlayerCameraReference;
+            Ray[] rays = new Ray[1];
+            for (int i = 0; i < rays.Length; i++)
+            {
+                rays[i] = new Ray(cam.position + cam.forward, cam.forward);
+            }
+
+            RaycastHit[] hits = new RaycastHit[1];
+            bool[] didHit = new bool[hits.Length];
+            for (int i = 0; i < hits.Length; i++)
+            {
+                didHit[i] = Physics.Raycast(rays[i], out hits[i], 500f, PlayerLayerMask);
+            }
+
+            bool confirm = false;
+            WeaponManager weps = PlayerObject.GetComponent<WeaponManager>();
+            for (int i = 0; i < hits.Length; i++)
+            {
+                if (!didHit[i])
+                {
+                    continue;
+                }
+
+                HitboxIdentity hitbox = hits[i].collider.GetComponent<HitboxIdentity>();
+                if (hitbox != null)
+                {
+                    GameObject parent = hits[i].collider.GetComponentInParent<NetworkIdentity>().gameObject;
+                    CharacterClassManager hitCcm = parent.GetComponent<CharacterClassManager>();
+                    PlayerStats stats = parent.GetComponent<PlayerStats>();
+
+                    if (weps.GetShootPermission(hitCcm))
+                    {
+                        stats.HurtPlayer(
+                            new PlayerStats.HitInfo(
+                                PluginMain.damage,
+                                PlayerObject.GetComponent<NicknameSync>().MyNick + " (" + PlayerObject.GetComponent<CharacterClassManager>().UserId + ")",
+                                DamageTypes.Com15,
+                                PlayerObject.GetComponent<QueryProcessor>().PlayerId
+                            ),
+                            parent);
+
+                        weps.RpcPlaceDecal(true, hitCcm.Classes.SafeGet(hitCcm.CurClass).bloodType, hits[i].point + hits[i].normal * 0.01f, Quaternion.FromToRotation(Vector3.up, hits[i].normal));
+                        confirm = true;
+                    }
+
+                    continue;
+                }
+
+                BreakableWindow window = hits[i].collider.GetComponent<BreakableWindow>();
+                if (window != null)
+                {
+                    window.ServerDamageWindow(PluginMain.damage);
+                    confirm = true;
+                    continue;
+                }
+
+                weps.RpcPlaceDecal(false, weps.curWeapon, hits[i].point + hits[i].normal * 0.01f, Quaternion.FromToRotation(Vector3.up, hits[i].normal));
+            }
+
+            int shots = 1;
+            for (int i = 0; i < shots; i++)
+            {
+                weps.RpcConfirmShot(confirm, weps.curWeapon);
+            }
+
+        }
+    }
+}
---
+++ Knife/Knife/PluginMain.cs
@@ -0,0 +1,72 @@
+﻿using EXILED;
+using EXILED.Extensions;
+using ItemManager.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knife
+{
+    public class PluginMain : Plugin
+    {
+        public override string getName => "Knife";
+        public CustomItemHandler<KnifeItem> knife;
+        public bool gunLoaded;
+
+        public static float damage;
+
+        public override void OnDisable()
+        {
+
+        }
+
+        public void RefreshConfig()
+        {
+            damage = Config.GetFloat("knife_dmg", 25f);
+        }
+
+        public override void OnEnable()
+        {
+            if (!Config.GetBool("knife_enabled", true))
+                return;
+            gunLoaded = false;
+            RefreshConfig();
+            EXILED.Events.WaitingForPlayersEvent += Events_WaitingForPlayersEvent;
+            Events.UseMedicalItemEvent += Events_UseMedicalItemEvent;
+            Log.Info("Knife plugin loaded.");
+        }
+
+        private void Events_UseMedicalItemEvent(MedicalItemEvent ev)
+        {
+            Log.Info("Medkit!!");
+        }
+
+        private void Events_WaitingForPlayersEvent()
+        {
+            if (gunLoaded)
+                return;
+            gunLoaded = true;
+            knife = new CustomItemHandler<KnifeItem>(207)
+            {
+                DefaultType = ItemType.Adrenaline
+            };
+            knife.Register();
+            /*foreach (var item in PlayerManager.localPlayer.GetComponent<ConsumableAndWearableItems>().usableItems)
+            {
+                Log.Info(item.inventoryID.ToString());
+            }*/
+            Log.Info("New Item Knife registered.");
+        }
+        public override void OnReload()
+        {
+            if (!Config.GetBool("knife_enabled", true))
+                return;
+            EXILED.Events.WaitingForPlayersEvent -= Events_WaitingForPlayersEvent;
+            Events.UseMedicalItemEvent -= Events_UseMedicalItemEvent;
+            knife.Unregister();
+            knife = null;
+        }
+    }
+}
